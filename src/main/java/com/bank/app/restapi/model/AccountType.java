7439a77c04d986328b98be469a0dcd239080fd89
Manol Draganov <678785@student.inholland.nl>
2023-05-13 16:18:08 +0200

    Transaction model + TransactionType (enum)

--- src/main/java/com/bank/app/restapi/model/AccountType.java
+++ src/main/java/com/bank/app/restapi/model/AccountType.java
@@ -3,5 +3,4 @@ package com.bank.app.restapi.model;
 public enum AccountType {
     SAVINGS,
     CURRENT
-
 }
---
+++ src/main/java/com/bank/app/restapi/model/Transaction.java
@@ -0,0 +1,50 @@
+package com.bank.app.restapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="transactions")
+public class Transaction {
+    @Id
+    @Column(name = "uuid")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "fromIBAN", referencedColumnName = "IBAN")
+    private Account fromIban;
+
+    @ManyToOne
+    @JoinColumn(name = "toIBAN", referencedColumnName = "IBAN")
+    private Account toIban;
+
+    @Column(name = "amount")
+    private float amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "typeOfTransaction")
+    private TransactionType typeOfTransaction;
+
+    @Column(name = "dateOfExecution")
+    private LocalDateTime dateOfExecution;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnoreProperties("transactions")
+    private User performingUser;
+
+    @Column(name = "description")
+    private String description;
+}
