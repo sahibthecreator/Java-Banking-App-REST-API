1915f93d71e58d713b551e17014e4cbedb727869
Achilleas Ballanos <686446@student.inholland.nl>
2023-05-19 00:18:42 +0200

    modified account controller and mapper

--- src/main/java/com/bank/app/restapi/controller/AccountController.java
+++ src/main/java/com/bank/app/restapi/controller/AccountController.java
@@ -1,93 +1,88 @@
 package com.bank.app.restapi.controller;
 
-import com.bank.app.restapi.http.HttpBody;
+import com.bank.app.restapi.dto.AccountDTO;
+import com.bank.app.restapi.dto.mapper.AccountMapper;
 import com.bank.app.restapi.model.Account;
 import com.bank.app.restapi.service.AccountService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("accounts")
 public class AccountController {
 
-    private final AccountService service;
+    private final AccountService accountService;
 
-    public AccountController(AccountService service){
-        this.service = service;
+    private AccountMapper accountMapper;
+
+    public AccountController(AccountService accountService){
+        this.accountService = accountService;
     }
 
-    @GetMapping
-    public ResponseEntity<?> allAccounts(){
+    @GetMapping("")
+    public ResponseEntity allAccounts(){
         try{
-            List<Account> accounts = service.getAllAccounts();
-            return ResponseEntity
-                    .ok()
-                    .body(new HttpBody<>(true, accounts));
+            List<AccountDTO> accounts = accountService.getAllAccounts().stream().map(accountMapper::toDTO).toList();
+            return ResponseEntity.status(200).body(accounts);
         }catch (Exception e){
-            return ResponseEntity
-                    .status(404)
-                    .body(new HttpBody<>(false, e.getMessage()));
+            return ResponseEntity.status(500).build();
         }
     }
 
     @PostMapping
-    public ResponseEntity createAccount(@RequestBody Account account){
+    public ResponseEntity<?> createAccount(@RequestBody AccountDTO accountDTO){
         try{
-            Account newAccount = service.createAccount(account);
-            return ResponseEntity.status(HttpStatus.CREATED).body(
-                    new HttpBody<>(true, newAccount));
-        }catch (Exception e){
-            return ResponseEntity
-                    .status(400)
-                    .body(new HttpBody<>(false, e.getMessage()));
+            Account account = accountMapper.toEntity(accountDTO);
+            Account newAccount = accountService.createAccount(account);
+            AccountDTO newAccountDTO = accountMapper.toDTO(newAccount);
+            return ResponseEntity.status(201).body(newAccountDTO);
+        }catch (Exception e) {
+            if (e instanceof IllegalArgumentException) {
+                return ResponseEntity.status(400).body("Invalid request body" + e.getMessage()); // Return 400 for bad
+                // request
+            } else {
+                return ResponseEntity.status(500).body("An error occurred: " + e.getMessage()); // Return 500 Internal
+                // Server Error
+            }
         }
     }
 
-    @GetMapping("/{iban}/info")
+    @GetMapping("/{iban}")
     public ResponseEntity accountInfo(@PathVariable String iban){
         try{
-            return ResponseEntity.ok()
-                    .body(new HttpBody<>(true, service.getAccountByIban(iban)));
-        }catch (Exception e){
-            return ResponseEntity
-                    .status(404)
-                    .body(new HttpBody<>(false, e.getMessage()));
+            Account account = accountService.getAccountByIban(iban);
+            AccountDTO accountDTO = accountMapper.toDTO(account);
+            return ResponseEntity.status(200).body(accountDTO);
+        }catch(Exception e){
+            return ResponseEntity.status(404).body(e.getMessage());
         }
     }
 
-    @GetMapping("/{iban}/info-balance")
+    @GetMapping("/{iban}")
     public ResponseEntity accountBalance(@PathVariable String iban){
         try{
-            return ResponseEntity.ok()
-                    .body(new HttpBody<>(true, Collections.singletonMap("balance", service.getBalanceByIban(iban))));
-        }catch (Exception e){
-            return ResponseEntity
-                    .status(404)
-                    .body(new HttpBody<>(false, e.getMessage()));
+            double balance = accountService.getBalanceByIban(iban);
+            return ResponseEntity.status(200).body(balance);
+        }catch(Exception e){
+            return ResponseEntity.status(404).body(e.getMessage());
         }
     }
 
-    @PatchMapping("/{iban}/deactivate")
+    @PatchMapping("/{iban}")
     public ResponseEntity deactivateAccount(@PathVariable String iban){
         try{
-            Account existingAccount = service.getAccountByIban(iban);
-            if(existingAccount == null){
-                return ResponseEntity
-                        .ok()
-                        .body(new HttpBody<>(false, "Account not found"));
+            Account account = accountService.getAccountByIban(iban);
+            if(account == null){
+                return ResponseEntity.status(404).body("Account not found");
             }
-            service.deactivateAccount(existingAccount);
-            return ResponseEntity
-                    .ok()
-                    .body(new HttpBody<>(true, "Account deactivated"));
+
+            accountService.deactivateAccount(account);
+            return ResponseEntity.status(200).body( "Account deactivated");
         }catch (Exception e){
             return ResponseEntity
-                    .status(400)
-                    .body(new HttpBody<>(false, e.getMessage()));
+                    .status(400).body(e.getMessage());
         }
     }
 
--- src/main/java/com/bank/app/restapi/dto/mapper/AccountMapper.java
+++ src/main/java/com/bank/app/restapi/dto/mapper/AccountMapper.java
@@ -2,29 +2,11 @@ package com.bank.app.restapi.dto.mapper;
 
 import com.bank.app.restapi.dto.AccountDTO;
 import com.bank.app.restapi.model.Account;
-import org.modelmapper.ModelMapper;
+import org.mapstruct.Mapper;
 
-import java.text.ParseException;
+@Mapper(componentModel = "spring")
+public interface AccountMapper {
+    AccountDTO toDTO(Account account);
+    Account toEntity(AccountDTO accountDTO);
 
-public class AccountMapper {
-
-    private ModelMapper mapper;
-
-    public AccountMapper(ModelMapper mapper) {
-        this.mapper = mapper;
-    }
-
-    public AccountDTO toDTO(Account a) {
-        return mapper.map(a, AccountDTO.class);
-    }
-
-    public Account toEntity(AccountDTO a) throws ParseException {
-        Account account = mapper.map(a, Account.class);
-        // set custom vars here
-
-        if (account.getId() != null) {
-            // code here...
-        }
-        return account;
-    }
 }
