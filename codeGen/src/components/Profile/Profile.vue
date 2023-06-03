e9b129fd919cc1676d0c7f5dfa77d51e8dd61505
Achilleas Ballanos <686446@student.inholland.nl>
2023-06-03 16:15:51 +0200

    update profile.vue
    still got to update the user

--- codeGen/src/components/Profile/Profile.vue
+++ codeGen/src/components/Profile/Profile.vue
@@ -7,13 +7,20 @@
             <span id="logoTitle">WAVR</span>
         </b-navbar-brand>
 
+        <div class="toggle-container">
+            <h6 class="toggle-title">Receive notifications</h6>
+            <div class="toggle-switch" :class="{ 'enabled': notificationsEnabled }" @click="toggleNotifications">
+                <div class="circle"></div>
+            </div>
+        </div>
+
         <b-navbar-nav class="ml-auto">
             <b-nav-item to="/dashboard"><img src="@/assets/Transaction/x.svg" /></b-nav-item>
         </b-navbar-nav>
     </div>
     <div class="profilePage">
         <div class="personalDetailsPanel">
-            <span>My personal details</span>
+            <span>PERSONAL DETAILS</span>
             <div class="personalDataContainer">
                 <h6>First name:<p class="personalData">{{ user ? user.firstName : "" }}</p>
                 </h6>
@@ -23,32 +30,62 @@
                 </h6>
                 <h6>Date of birth:<p class="personalData"> {{ user ? user.dateOfBirth : "" }}</p>
                 </h6>
-                <h6>Email: <input v-model="user.email" type="text" class="personalData">
+                <h6>Email: <input :value="user.email" type="text" class="personalData">
                 </h6>
-                <h6>Password: <input v-model="user.password" type="password" class="personalData" ref="passwordInput">
-                </h6>
+                <!-- <h6>Password: <input v-model="user.password" type="password" class="personalData" ref="passwordInput">
+                </h6> -->
             </div>
-            <b-button variant="dark_primary" v-on:click="updateUserDataBtn()">Update my data</b-button>
+            <b-button class="button" variant="dark_primary" v-on:click="updateUserDataBtn()">Update my data</b-button>
             <p class="text-danger errorMsg">{{ errorMsg }}</p>
             <span class="tos">You may change your email and password, but for other changes on personal data please call the
                 bank.</span>
         </div>
         <div class="accountsPanel">
-            <span>My accounts</span>
+            <span>MY ACCOUNTS</span>
             <div class="accountsContainer">
-                <b-table :items="accounts" :fields="accountFields"></b-table>
+                <b-table class="table" :items="accounts" :fields="accountFields"></b-table>
             </div>
-            <b-button variant="dark_primary" v-on:click="requestAccountBtn()">Request new account</b-button>
+            <b-button class="button" variant="dark_primary" v-on:click="requestAccountBtn()">Request new account</b-button>
             <p class="text-danger errorMsg">{{ errorMsg }}</p>
             <span class="tos">These are all your accounts.</span>
         </div>
         <div class="transactionHistoryPanel">
-            <span>My transactions</span>
+            <span>MY TRANSACTIONS</span>
             <div class="transactionHistoryContainer">
-                <b-table :items="transaction" :fields="transactionFields"></b-table>
+                <b-table class="table" :items="transactions" :fields="transactionFields"></b-table>
             </div>
+            <b-button class="button" variant="dark_primary" v-on:click="makeTransactionBtn()">Make new
+                transaction</b-button>
             <p class="text-danger errorMsg">{{ errorMsg }}</p>
             <span class="tos">This is your transaction history for the past month.</span>
+        </div>
+    </div>
+
+    <!-- popup -->
+    <div class="card" v-if="responsePopupEnabled">
+        <div class="header">
+            <!-- Loading -->
+            <div class="spinnerContainer" v-if="loading">
+                <div class="spinner"></div>
+                <div class="loader">
+                    <div class="words">
+                        <span class="word">Updating your data</span>
+                        <span class="word">1 more second</span>
+                    </div>
+                </div>
+            </div>
+            <!-- /Loading -->
+
+            <div class="image" v-if="!loading">
+                <img src="@/assets/SuccessIcon.png" class="success icon" />
+            </div>
+            <div class="content" v-if="!loading">
+                <span class="title">Your data updated successfully</span>
+            </div>
+            <div class="actions">
+                <b-button variant="dark_primary" class="history" v-on:click="this.$router.push('/dashboard/profile')"
+                    v-if="!loading">Done</b-button>
+            </div>
         </div>
     </div>
 </template>
@@ -83,6 +120,11 @@ export default {
                 { key: 'amount', label: 'Amount' },
                 { key: 'dateOfExecution', label: 'Date' },
             ],
+            responsePopupEnabled: false,
+            loading: false,
+            delay: (ms) => new Promise((res) => setTimeout(res, ms)),
+            notificationsEnabled: false,
+
         };
     },
     mounted() {
@@ -91,14 +133,14 @@ export default {
         this.getTransactionsByUserId();
     },
     methods: {
-        togglePasswordVisibility() {
-            const passwordInput = this.$refs.passwordInput;
-            if (passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-            } else {
-                passwordInput.type = 'password';
-            }
-        },
+        // togglePasswordVisibility() {
+        //     const passwordInput = this.$refs.passwordInput;
+        //     if (passwordInput.type === 'password') {
+        //         passwordInput.type = 'text';
+        //     } else {
+        //         passwordInput.type = 'password';
+        //     }
+        // },
         async getUser() {
             try {
                 let user = await this.$store.dispatch('getUser', this.$store.getters.getUserId);
@@ -116,6 +158,15 @@ export default {
         },
         async getTransactionsByUserId() {
             try {
+                // let currentDate = new Date();
+                // let monthAgo = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate());
+
+                // let filteredTransactions = (await this.$store.dispatch('getTransactionsByUserId', this.$store.getters.getUserId)).filter(transaction => {
+                //     let transactionDate = new Date(transaction.dateOfExecution);
+                //     return transactionDate >= monthAgo;
+                // });
+                // this.transactions = await [filteredTransactions];
+
                 this.transactions = await this.$store.dispatch('getTransactionsByUserId', this.$store.getters.getUserId);
             } catch (error) {
                 this.errorMsg = error;
@@ -124,23 +175,52 @@ export default {
         async requestAccountBtn() {
             window.location.href = "/dashboard/requestAccount";
         },
+        async makeTransactionBtn() {
+            this.$router.push({ name: 'transfer', params: { accounts: "22" } });
+        },
         async updateUserDataBtn() {
             try {
+                console.log('start updating')
                 const updateUserData = {
-                    userId: this.$store.state.userId,
+                    firstName: this.user.firstName,
+                    lastName: this.user.lastName,
+                    bsn: this.user.bsn,
+                    dateOfBirth: this.user.dateOfBirth,
                     email: this.user.email,
                     password: this.user.password,
+                    role: this.user.role,
+                    dayLimit: this.user.dayLimit,
+                    transactionLimit: this.user.transactionLimit
 
                 };
-                await this.$store.dispatch('updateUser', updateUserData);
+
+                const currentUser = { ...this.user };
+                const updatedUser = {
+                    ...currentUser,
+                    ...updateUserData
+                };;
+
+                console.log('continue updating')
+
+                await this.$store.dispatch('updateUser', {
+                    userId: this.$store.state.userId,
+                    user: updatedUser
+                });
+
+                console.log('finish updating')
+
                 this.responsePopupEnabled = true;
                 this.loading = true;
                 await this.delay(1500);
                 this.loading = false;
+
             } catch (error) {
                 this.errorMsg = error;
             }
         },
+        toggleNotifications() {
+            this.notificationsEnabled = !this.notificationsEnabled;
+        }
     },
 };
 </script>
@@ -163,20 +243,20 @@ export default {
     background-repeat: repeat;
     background-clip: border-box;
     background-position-x: center;
-    background-size: cover;
+    background-size: 100% 100%;
     height: 100%;
     display: flex;
     flex-wrap: wrap;
-    // align-items: center;
-    // justify-content: center;
+    align-items: center;
+    justify-content: center;
     padding: 10px 0;
 
 }
 
 
-.personalDetailsPanel,
-.accountsPanel {
-    width: 36%;
+.personalDetailsPanel {
+    width: 20%;
+    margin: 20px auto;
     height: 80%;
     background: var(--white);
     min-height: fit-content;
@@ -186,89 +266,119 @@ export default {
     margin-left: 20px;
     display: flex;
     flex-direction: column;
-    padding: 20px;
+    padding: 40px;
+    align-items: center;
+}
+
+.accountsPanel {
+    width: 30%;
+    margin: 20px auto;
+    height: 80%;
+    background: var(--white);
+    min-height: fit-content;
+    border-radius: 35px;
+    background-color: var(--white);
+    margin-top: 100px;
+    margin-left: 20px;
+    display: flex;
+    flex-direction: column;
+    padding: 40px;
     align-items: center;
 }
 
 .transactionHistoryPanel {
-    background: var(--white);
-    min-width: fit-content;
-    width: 76%;
+    width: 40%;
+    margin: 20px auto;
     height: 80%;
+    background: var(--white);
     min-height: fit-content;
     border-radius: 35px;
     background-color: var(--white);
-    margin-top: 20px;
+    margin-top: 100px;
+    margin-left: 20px;
     display: flex;
     flex-direction: column;
-    padding: 30px;
+    padding: 40px;
     align-items: center;
 }
 
-.personalDataContainer {
-    display: flex;
-    flex-direction: column;
-    align-items: left;
-    margin: 10px;
-
+::v-deep .table tbody td {
+    font-size: small;
+    font-style: inherit;
+    color: var(--blue-dark);
 }
 
 .personalData {
-    font-size: 20px;
-    font-weight: 200;
+    font-size: small;
+    font-style: inherit;
     margin-top: 5px;
     color: var(--blue-dark);
 }
 
-
-.requestPanel>span:nth-child(1) {
-    font-size: 24px;
-}
 
 .tos {
     text-align: center;
     width: 60%;
     font-size: 12px;
     color: var(--gray-light);
-}
-
-.inputRequestContainer {
-    font-size: 16px;
-    padding: 10px 20px;
-    border-radius: 7px;
-    border: 1px var(--gray-dark) solid;
-    font-weight: 500;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    max-width: 350px;
-    width: 350px;
-    min-width: 300px;
-    margin: 0px auto;
-}
-
-.inputRequestContainer>span {
-    font-size: 16px;
-    font-weight: 500;
-}
-
-.inputRequestContainer>input {
-    font-size: 20px;
-    font-weight: 400;
-    border: none;
-    background: transparent;
-    color: var(--gray-light);
+    position: absolute;
+    bottom: 0;
+    margin-bottom: 5px;
 }
 
 
-.requestPanel>button {
-    max-width: 350px;
-    width: 350px;
-    min-width: 300px;
+.button {
+    position: absolute;
+    bottom: 0;
+    margin-bottom: 80px;
 }
 
-.inputRequestContainer>input::placeholder {
-    color: var(--gray-light);
+.toggle-container {
+    position: absolute;
+    right: 20%;
+    align-items: center;
+    margin-left: 20px;
+}
+
+.toggle-title {
+    font-size: small;
+    font-style: inherit;
+    margin-right: 10px;
+    // font-size: 14px;
+    font-weight: bold;
+    color: var(--blue-dark);
+
+}
+
+.toggle-switch {
+    position: relative;
+    width: 40px;
+    height: 20px;
+    border-radius: 10px;
+    background-color: #ccc;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.toggle-switch.enabled {
+    background-color: #2ecc71;
+}
+
+.circle {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    transition: left 0.3s, background-color 0.3s;
+}
+
+.toggle-switch.enabled .circle {
+    left: 22px;
+    background-color: #fff;
 }
 
 .card {
